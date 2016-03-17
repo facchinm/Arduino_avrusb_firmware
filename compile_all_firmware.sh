ffab17cdf9bb42efe8ce9fde0d5ea315cba0f979
Martino Facchin <m.facchin@arduino.cc>
2016-03-17 12:14:52 +0100

    add Yun101 firmwares

--- compile_all_firmware.sh
+++ compile_all_firmware.sh
@@ -18,10 +18,14 @@ make clean && ARDUINO_MODEL_PID=0x0242 make #Genuino Mega2560 R3
 mv Arduino-usbserial.hex ../distrib/GenuinoMega2560_16u2.hex
 cd ..
 
-echo "Compiling 32u4 bootloaders (Micro)"
+echo "Compiling 32u4 bootloaders (Micro and Yun101)"
 cd Caterina2
 make clean && ARDUINO_MODEL_PID=0x0037 make #Arduino Micro
 mv Caterina2.hex ../distrib/Caterina2_ArduinoMicro.hex
 make clean && ARDUINO_MODEL_PID=0x0237 make #Genuino Micro
 mv Caterina2.hex ../distrib/Caterina2_GenuinoMicro.hex
+make clean && ARDUINO_MODEL_PID=0x0051 make #Arduino Yun101
+mv Caterina2.hex ../distrib/Caterina2_ArduinoYun101.hex
+make clean && ARDUINO_MODEL_PID=0x0251 make #Genuino Yun101
+mv Caterina2.hex ../distrib/Caterina2_GenuinoYun101.hex
 cd ..
